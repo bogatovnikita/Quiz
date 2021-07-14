4b9db933cc309f8121ada5e1b3f9baeee4faf06e
Bogatov Nikita <bogatovnikita@gmail.com>
2021-07-14 21:32:52 +0300

    add new activity level1

--- app/src/main/java/com/bogatovnikita/quiz/MainActivity.java
+++ app/src/main/java/com/bogatovnikita/quiz/MainActivity.java
@@ -8,8 +8,11 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private long backPressedTime;
+    private Toast backToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +25,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 try {
                     Intent intent = new Intent(MainActivity.this, GameLevels.class);
-                    startActivity(intent);finish();
+                    startActivity(intent);
+                    finish();
                 } catch (Exception e) {
 
                 }
@@ -31,5 +35,18 @@ public class MainActivity extends AppCompatActivity {
 
         Window window = getWindow();
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            backToast.cancel();
+            super.onBackPressed();
+            return;
+        } else {
+            backToast = Toast.makeText(getBaseContext(), "Нажмите ещё раз, чтобы выйти", Toast.LENGTH_SHORT);
+            backToast.show();
+        }
+        backPressedTime = System.currentTimeMillis();
     }
 }
