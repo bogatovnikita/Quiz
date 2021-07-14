1009817f4c77644bd987a145959649ed6f47837e
Bogatov Nikita <bogatovnikita@gmail.com>
2021-07-14 21:16:02 +0300

    add GameLevels and writing code for two file

--- app/src/main/java/com/bogatovnikita/quiz/GameLevels.java
+++ app/src/main/java/com/bogatovnikita/quiz/GameLevels.java
@@ -1,8 +1,11 @@
 package com.bogatovnikita.quiz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,5 +18,30 @@ public class GameLevels extends AppCompatActivity {
 
         Window window = getWindow();
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Button buttonBack = findViewById(R.id.btn_back);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(GameLevels.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (Exception e) {
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        try {
+            Intent intent = new Intent(GameLevels.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } catch (Exception e) {
+
+        }
     }
 }
