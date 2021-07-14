083d5aaa3fce4879f9562af411954d947279fcc3
Bogatov Nikita <bogatovnikita@gmail.com>
2021-07-14 22:09:43 +0300

    add background level1

--- app/src/main/java/com/bogatovnikita/quiz/Level1.java
+++ app/src/main/java/com/bogatovnikita/quiz/Level1.java
@@ -3,6 +3,9 @@ package com.bogatovnikita.quiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
 
 public class Level1 extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class Level1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universal);
+
+
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
     }
 }
