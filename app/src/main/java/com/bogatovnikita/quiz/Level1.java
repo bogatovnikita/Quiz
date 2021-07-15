de5d751fe74e8047def497feed6a8adda0640250
Bogatov Nikita <bogatovnikita@gmail.com>
2021-07-15 09:26:42 +0300

    add tv for point

--- app/src/main/java/com/bogatovnikita/quiz/Level1.java
+++ app/src/main/java/com/bogatovnikita/quiz/Level1.java
@@ -3,6 +3,8 @@ package com.bogatovnikita.quiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Level1 extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class Level1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universal);
+
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
