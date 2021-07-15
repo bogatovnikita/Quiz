b48786a8e22f3b66a4076cb74d51c076789bbc60
Bogatov Nikita <bogatovnikita@gmail.com>
2021-07-15 17:15:59 +0300

    add two image for level1

--- app/src/main/java/com/bogatovnikita/quiz/Level1.java
+++ app/src/main/java/com/bogatovnikita/quiz/Level1.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class Level1 extends AppCompatActivity {
 
@@ -12,6 +13,12 @@ public class Level1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universal);
+
+        final ImageView imgLeft = findViewById(R.id.img_left);
+        imgLeft.setClipToOutline(true);
+
+        final ImageView imgRight = findViewById(R.id.img_right);
+        imgRight.setClipToOutline(true);
 
         Window window = getWindow();
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
