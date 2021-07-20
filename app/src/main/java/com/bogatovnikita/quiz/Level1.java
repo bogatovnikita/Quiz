04d3b623626fe5e1f7ac58ed39c673c783e6d46f
Bogatov Nikita <bogatovnikita@gmail.com>
2021-07-20 19:58:03 +0300

    add configuration

--- app/src/main/java/com/bogatovnikita/quiz/Level1.java
+++ app/src/main/java/com/bogatovnikita/quiz/Level1.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -98,6 +99,11 @@ public class Level1 extends AppCompatActivity {
             }
         });
 
+        final int[] progress = {R.id.point1, R.id.point2, R.id.point3, R.id.point4, R.id.point5,
+                R.id.point6, R.id.point7, R.id.point8, R.id.point9, R.id.point10, R.id.point11,
+                R.id.point12, R.id.point13, R.id.point14, R.id.point15, R.id.point16,
+                R.id.point17, R.id.point18, R.id.point19, R.id.point20};
+
         final Animation a = AnimationUtils.loadAnimation(Level1.this, R.anim.alpha);
         numLeft = random.nextInt(10);
         imgLeft.setImageResource(array.imageOne[numLeft]);
@@ -111,6 +117,41 @@ public class Level1 extends AppCompatActivity {
 
         imgRight.setImageResource(array.imageOne[numRight]);
         textRight.setText(array.textOne[numRight]);
+
+        imgLeft.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    imgRight.setEnabled(false);
+                    if (numLeft > numRight) {
+                        imgLeft.setImageResource(R.drawable.img_true);
+                    } else {
+                        imgLeft.setImageResource(R.drawable.img_false);
+                    }
+
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+
+                    if (numLeft > numRight) {
+                        if (count < 20) {
+                            count++;
+                        }
+                        for (int i = 0; i < 20; i++) {
+                            TextView textView = findViewById(progress[i]);
+                            textView.setBackgroundResource(R.drawable.style_points);
+                        }
+                        for (int i = 0; i < count; i++) {
+                            TextView textView = findViewById(progress[i]);
+                            textView.setBackgroundResource(R.drawable.style_points_green);
+
+                        }
+                    } else {
+
+                    }
+                }
+                return true;
+            }
+        });
     }
 
     @Override
